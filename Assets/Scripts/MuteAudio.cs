4c80b18f389f40cedcdb6ffc90a791e4c4f8dc1b
Kirage <tommi.m@hotmail.com>
2020-01-03 10:44:32 +0200

    bigbomb animation

---
+++ Assets/Scripts/MuteAudio.cs
@@ -0,0 +1,22 @@
+﻿// Mutes-Unmutes the sound from this object each time the user presses space.
+using UnityEngine;
+using System.Collections;
+
+public class MuteAudio : MonoBehaviour
+{
+    [SerializeField]
+    private AudioSource efxSource;    
+    [SerializeField]
+    private AudioSource musicSource;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+            efxSource.mute = !efxSource.mute;
+            musicSource.mute = !musicSource.mute;
+    }
+}
